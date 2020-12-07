757d40d63b90e4b2d018a49ccd92ebba6ce0637f
Gluhenko Daniil <gluhenko-dv@yandex.ru>
2020-12-07 18:55:54 +0300

    feat const tasks

--- js/main.js
+++ js/main.js
@@ -5,12 +5,16 @@ const inputText = document.querySelector('.input-text'),
   inputTime = document.querySelector('.input-time'),
   addForm = document.querySelector('.add-form'),
   todoList = document.querySelector('.todo-list'),
+  constList = document.querySelector('.const-list'),
   todoCompleted = document.querySelector('.todo-completed');
 
+let alertAudio = new Audio(),
+  notificationAudio = new Audio();
+alertAudio.src = '../sound/alert.mp3';
+notificationAudio.src = '../sound/notification.mp3';
+
 const todoItemBackground = ['#05EFCC', '#8FE3C5', '#9949D4', '#3A45CC', '#9B250E', '#ED93DE', '#D6C7BA', '#F79680', '#E4D20C', '#92EE83', '#D07977', '#1BC7FB', '#F37274', '#79D49A'];
-let todoData;
-
-
+let todoData, constData;
 
 const renderList = () => {
   todoList.textContent = '';
@@ -21,21 +25,19 @@ const renderList = () => {
   todoData.forEach(item => {
     const li = document.createElement('li');
     li.classList.add('todo-item');
+    li.draggable = true;
     li.style.backgroundColor = item.background;
     li.innerHTML = `
     <span class="item-title">${item.name}</span>
-    <span class="item-time">${item.time}</span>
     <button class="btn btn-delete"></button>
-    <button class="btn btn-complete"></button>
     `;
     if (item.completed) {
       todoCompleted.append(li);
     } else {
       todoList.append(li);
     }
-
-    const todoCompletedBtn = li.querySelector('.btn-complete');
-    todoCompletedBtn.addEventListener('click', () => {
+    const todoCompletedBtn = li;
+    todoCompletedBtn.addEventListener('dblclick', () => {
       item.completed = !item.completed;
       let json = JSON.stringify(todoData);
       localStorage.todoData = json;
@@ -43,7 +45,6 @@ const renderList = () => {
     });
     const todoRemovedBtn = li.querySelector('.btn-delete');
     todoRemovedBtn.addEventListener('click', () => {
-      if (!confirm('Вы уверены?')) return;
       todoData.splice(todoData.indexOf(item), 1);
       let json = JSON.stringify(todoData);
       localStorage.todoData = json;
@@ -51,19 +52,49 @@ const renderList = () => {
     });
   });
 };
+const renderConstList = () => {
+  constList.textContent = '';
+  constData = localStorage.constData ? JSON.parse(localStorage.constData) : [];
+  constData.forEach(item => {
+    const li = document.createElement('li');
+    li.classList.add('const-item');
+    li.draggable = true;
+    li.style.backgroundColor = item.background;
+    li.innerHTML = `
+    <span class="item-title">${item.name}</span>
+    <button class="btn btn-delete"></button>
+    `;
 
+    constList.append(li);
+
+    const constCompletedBtn = li;
+    constCompletedBtn.addEventListener('dblclick', () => {
+      item.completed = !item.completed;
+      let json = JSON.stringify(constData);
+      localStorage.constData = json;
+      renderList();
+    });
+    const constRemovedBtn = li.querySelector('.btn-delete');
+    constRemovedBtn.addEventListener('click', () => {
+      constData.splice(constData.indexOf(item), 1);
+      let json = JSON.stringify(constData);
+      localStorage.constData = json;
+      renderList();
+    });
+  });
+};
 
 addForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (!inputText.value || !inputDate || !inputTime) {
-    alert('Заполни поле ввода');
+  if (!inputText.value) {
+    alertAudio.play();
+    inputText.style.borderBottom = '2px solid red';
     return;
   }
+  inputText.style.borderBottom = '1px solid rgb(136, 136, 136)';
   const randColor = Math.floor(Math.random() * todoItemBackground.length);
   const newTodo = {
     name: inputText.value,
-    date: inputDate.value,
-    time: inputTime.value,
     background: todoItemBackground[randColor],
     completed: false
   };
@@ -76,3 +107,4 @@ addForm.addEventListener('submit', (event) => {
 
 
 renderList();
+renderConstList();
